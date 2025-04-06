82e804f38dbfb50498b8ef22b6cb3fdbc1e76d2d
Plovchikdeval <plov99502@gmail.com>
2025-04-06 15:30:47 +0500

    beta axyetb

--- herokutl/tl/tlobject.py
+++ herokutl/tl/tlobject.py
@@ -9,18 +9,14 @@ _EPOCH_NAIVE_LOCAL = datetime(*time.localtime(0)[:6])
 _EPOCH = _EPOCH_NAIVE.replace(tzinfo=timezone.utc)
 
 
+FORBIDDEN_CONSTRUCTORS = [0x418d4e0b]
+FORBIDDEN_SUBCLASSES = [0xf5b399ac]
+
 def _datetime_to_timestamp(dt):
-    # If no timezone is specified, it is assumed to be in utc zone
     if dt.tzinfo is None:
         dt = dt.replace(tzinfo=timezone.utc)
-    # We use .total_seconds() method instead of simply dt.timestamp(), 
-    # because on Windows the latter raises OSError on datetimes ~< datetime(1970,1,1)
     secs = int((dt - _EPOCH).total_seconds())
-    # Make sure it's a valid signed 32 bit integer, as used by Telegram.
-    # This does make very large dates wrap around, but it's the best we
-    # can do with Telegram's limitations.
     return struct.unpack('i', struct.pack('I', secs & 0xffffffff))[0]
-
 
 def _json_default(value):
     if isinstance(value, bytes):
@@ -30,21 +26,28 @@ def _json_default(value):
     else:
         return repr(value)
 
-
 class TLObject:
     CONSTRUCTOR_ID = None
     SUBCLASS_OF_ID = None
 
+    def __new__(cls, *args, **kwargs):
+        if cls.CONSTRUCTOR_ID in FORBIDDEN_CONSTRUCTORS and cls.SUBCLASS_OF_ID in FORBIDDEN_SUBCLASSES:
+            raise ValueError(
+                f"Instantiation of {cls.__name__} is forbidden due to its CONSTRUCTOR_ID and SUBCLASS_OF_ID."
+            )
+        elif cls.SUBCLASS_OF_ID in FORBIDDEN_SUBCLASSES:
+            pass
+        elif cls.CONSTRUCTOR_ID in FORBIDDEN_CONSTRUCTORS:
+            raise ValueError(
+                f"Instantiation of {cls.__name__} is forbidden due to its CONSTRUCTOR_ID."
+            )
+        return super().__new__(cls)
+
     @staticmethod
     def pretty_format(obj, indent=None):
-        """
-        Pretty formats the given object as a string which is returned.
-        If indent is None, a single line will be returned.
-        """
         if indent is None:
             if isinstance(obj, TLObject):
                 obj = obj.to_dict()
-
             if isinstance(obj, dict):
                 return '{}({})'.format(obj.get('_', 'dict'), ', '.join(
                     '{}={}'.format(k, TLObject.pretty_format(v))
@@ -77,15 +80,13 @@ class TLObject:
                         result.append('=')
                         result.append(TLObject.pretty_format(v, indent))
                         result.append(',\n')
-                    result.pop()  # last ',\n'
+                    result.pop()
                     indent -= 1
                     result.append('\n')
                     result.append('\t' * indent)
                 result.append(')')
-
             elif isinstance(obj, str) or isinstance(obj, bytes):
                 result.append(repr(obj))
-
             elif hasattr(obj, '__iter__'):
                 result.append('[\n')
                 indent += 1
@@ -96,7 +97,6 @@ class TLObject:
                 indent -= 1
                 result.append('\t' * indent)
                 result.append(']')
-
             else:
                 result.append(repr(obj))
 
@@ -104,7 +104,6 @@ class TLObject:
 
     @staticmethod
     def serialize_bytes(data):
-        """Write bytes by using Telegram guidelines"""
         if not isinstance(data, bytes):
             if isinstance(data, str):
                 data = data.encode('utf-8')
@@ -149,7 +148,6 @@ class TLObject:
         elif isinstance(dt, float):
             dt = int(dt)
         elif isinstance(dt, timedelta):
-            # Timezones are tricky. datetime.utcnow() + ... timestamp() works
             dt = _datetime_to_timestamp(datetime.utcnow() + dt)
 
         if isinstance(dt, int):
@@ -173,16 +171,6 @@ class TLObject:
         raise NotImplementedError
 
     def to_json(self, fp=None, default=_json_default, **kwargs):
-        """
-        Represent the current `TLObject` as JSON.
-
-        If ``fp`` is given, the JSON will be dumped to said
-        file pointer, otherwise a JSON string will be returned.
-
-        Note that bytes and datetimes cannot be represented
-        in JSON, so if those are found, they will be base64
-        encoded and ISO-formatted, respectively, by default.
-        """
         d = self.to_dict()
         if fp:
             return json.dump(d, fp, default=default, **kwargs)
@@ -193,15 +181,8 @@ class TLObject:
         try:
             return self._bytes()
         except AttributeError:
-            # If a type is wrong (e.g. expected `TLObject` but `int` was
-            # provided) it will try to access `._bytes()`, which will fail
-            # with `AttributeError`. This occurs in fact because the type
-            # was wrong, so raise the correct error type.
             raise TypeError('a TLObject was expected but found something else')
 
-    # Custom objects will call `(...)._bytes()` and not `bytes(...)` so that
-    # if the wrong type is used (e.g. `int`) we won't try allocating a huge
-    # amount of data, which would cause a `MemoryError`.
     def _bytes(self):
         raise NotImplementedError
 
@@ -211,9 +192,6 @@ class TLObject:
 
 
 class TLRequest(TLObject):
-    """
-    Represents a content-related `TLObject` (a request that can be sent).
-    """
     @staticmethod
     def read_result(reader):
         return reader.tgread_object()
